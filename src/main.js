2fe44039c8ea02b9c526d1a815c10662734866bc
Brave-Y <1175151069@qq.com>
2021-05-12 16:59:04 +0800

    完成taBar

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// 导入vant
-import Vant from 'vant'
+// 导入vant, 字体图标
+import Vant, { Icon } from 'vant'
 // 引入vantcss
 import 'vant/lib/index.css'
 // 它能根据的手机尺寸来调整rem的基准值
 import 'amfe-flexible'
 // 注册vant
 Vue.use(Vant)
-
+// 注册字体图标
+Vue.use(Icon)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    我是home页面
-    <router-view/>
+    <router-view />
+    <van-tabbar v-model="active" fixed z-index route @change="onChange">
+      <van-tabbar-item icon="home-o" to="/index">首页</van-tabbar-item>
+      <van-tabbar-item icon="apps-o" dot to="/list">分类</van-tabbar-item>
+      <van-tabbar-item icon="cart-o" :badge=badge to="/cart">购物车</van-tabbar-item>
+      <van-tabbar-item icon="contact" badge="20" to="/my">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Index',
+  data () {
+    return {
+      // 当前选中项
+      active: 0,
+      // 购物车数量
+      badge: 6
+    }
+  },
+  methods: {
+    onChange (item) {
+      console.log(item)
+      this.active = item
+    }
+  }
+}
 </script>
 
 <style>
