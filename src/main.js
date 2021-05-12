6f5e51db65f692d0272ff73700d630cb63ad08f3
Brave-Y <1175151069@qq.com>
2021-05-12 16:03:56 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入vant
+import Vant from 'vant'
+// 引入vantcss
+import 'vant/lib/index.css'
+// 它能根据的手机尺寸来调整rem的基准值
+import 'amfe-flexible'
+// 注册vant
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
