5bc94863f1373c0c62416e1b0eb8d9a04a3b9e80
Brave-Y <1175151069@qq.com>
2021-05-12 17:50:02 +0800

    轮播图区域

---
+++ src/utils/Sql/index.js
@@ -0,0 +1,23 @@
+const Swipers = {
+  data: [
+    {
+      image_src: 'https://emm.erm.icu/myvant/666.jpg',
+      navigator_url: 'https://www.baidu.com/'
+    },
+    {
+      image_src: 'https://emm.erm.icu/myvant/666.jpg',
+      navigator_url: 'https://www.baidu.com/'
+    },
+    {
+      image_src: 'https://emm.erm.icu/myvant/666.jpg',
+      navigator_url: 'https://www.baidu.com/'
+    },
+    {
+      image_src: 'https://emm.erm.icu/myvant/666.jpg',
+      navigator_url: 'https://www.baidu.com/'
+    }
+  ],
+  meta: { msg: '获取成功', status: 200 }
+}
+
+module.exports = { Swipers }
--- src/views/Index/index.vue
+++ src/views/Index/index.vue
@@ -1,13 +1,51 @@
 <template>
-  <div>我是index页面</div>
+  <div>
+
+    <!-- 轮播图区域 -->
+    <div class="swipe">
+    <van-swipe class="my-swipe" :autoplay="2400" loop touchable  indicator-color="white">
+      <van-swipe-item v-for="(item, index) in swipers" :key="index">
+        <a >
+          <!-- 超链接项目完成再添加 -->
+           <!-- :href=item.navigator_url -->
+          <img :src=item.image_src>
+        </a>
+      </van-swipe-item>
+    </van-swipe>
+    </div>
+
+    <!-- 导航栏区域 -->
+6
+  </div>
 </template>
 
 <script>
+// 轮播图数据
+import { Swipers } from '@/utils/Sql/index.js'
 export default {
-
+  data () {
+    return {
+      // 轮播图数据
+      swipers: []
+    }
+  },
+  created () {
+    this.swipers = Swipers.data
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .my-swipe .van-swipe-item {
+    color: #fff;
+    font-size: 20px;
+    line-height: 16px;
+    text-align: center;
+    background-color: #39a9ed;
+  }
+  .swipe img{
+    display: inline-block;
+    width: 100%;
+    height: 120px;
+}
 </style>
