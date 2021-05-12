9d10276964f2790c2820512022c853f199ec7fab
Brave-Y <1175151069@qq.com>
2021-05-12 16:36:05 +0800

    ‘路由建立完成’

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,64 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// 引入home页面
+import Home from '../views/Home'
+// 引入主页
+import Index from '../views/Index'
+// 引入列表页
+import List from '../views/List'
+// 引入购物车
+import Cart from '../views/Cart'
+// 引入我的
+import My from '../views/My'
+// 引入注册页面
+import Login from '../views/Login'
+// 引入404页面
+import Error from '../views/Error'
 
+// 注册路由
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '',
+    redirect: '/index'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    name: 'Home',
+    component: Home,
+    children: [
+      {
+        path: 'index',
+        name: 'Index',
+        component: Index
+      },
+      {
+        path: 'list',
+        name: 'Name',
+        component: List
+      },
+      {
+        path: 'cart',
+        name: 'Cart',
+        component: Cart
+      },
+      {
+        path: 'my',
+        name: 'My',
+        component: My
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '*',
+    name: 'Error',
+    component: Error
   }
 ]
 
