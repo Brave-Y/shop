34566df614df165950cea25c924cdeb8119af8dd
Brave-Y <1175151069@qq.com>
2021-05-15 22:55:29 +0800

    本地存储bug

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -6,7 +6,7 @@ export default {
     // 购物车的数组，用来存储购物车中每个商品的信息对象
     // 每个商品的信息对象，都包含如下 6 个属性：
     // { goods_id, goods_name, goods_price, goods_count, goods_small_logo, goods_state }
-    cart: JSON.parse(localStorage.getItem('cart') || []),
+    cart: localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [],
     name: 6
   }),
   mutations: {
